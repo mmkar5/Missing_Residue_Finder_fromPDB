695fab89033faa8cf0cf18071f4491994c708875
Madhabendra Mohon Kar <160476301+mmkar5@users.noreply.github.com>
2024-05-06 10:52:26 +0530

    PDB_Missing_Residue.py

--- PDB_Missing_Residue.py
+++ PDB_Missing_Residue.py
@@ -125,7 +125,7 @@ def get_args():
         "-o",
         required=False,
         type=str,
-        help="Enter output filepath, if ouput to be saved in a file",
+        help="Enter output filepath, if output to be saved in a file",
     )
     parser.add_argument(
         "-seq",
